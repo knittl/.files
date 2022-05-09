f579ca36c3a24799c6a38ff8436870c240276dc8
Daniel Knittl-Frank <knittl89+git@googlemail.com>
2022-05-09 16:16:34 +0200

    Tune bash prompt (exit code, git info)

--- .git.bash
+++ .git.bash
@@ -13,8 +13,17 @@ GIT_PS1_SHOWUNTRACKEDFILES=1
 #GIT_PS1_SHOWUPSTREAM="verbose"
 
 ## extend regular prompt with git-prompt
-PROMPT_COMMAND='ec=$?;'"${PROMPT_COMMAND:-:};"'__git_ps1 "$(fmt_exit_code $ec)'${PS1%'\$ '}'" "\n\\$ "'
-fmt_exit_code() { test "$1" -ne 0 && printf '\033[31m✘ \033[0m' "$1"; }
+_orig_ps1="$PS1"
+__fmt_exit_code() { test "$1" -ne 0 && printf '\[\033[31m\]✘ \[\033[0m\]' "$1"; }
+__show_exit_code() {
+	local dollar='\$ '
+	PS1="$(__fmt_exit_code $1)${_orig_ps1%"$dollar"}$dollar"
+}
+__git_prompt(){
+	local dollar='\$ '
+	__git_ps1 "${PS1%"$dollar"}" "\n$dollar"
+}
+PROMPT_COMMAND="ec=\$?;${PROMPT_COMMAND:-:};__show_exit_code \$ec;__git_prompt"
 
 ## single letters are so neat!
 alias g='git'
